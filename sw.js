fbc824f7abd009ac92064a2247b395e3a3c3e99e
rreppi213-a11y <rreppi213@naver.com>
2026-02-23 17:10:25 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'transit-pwa-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+// Install: cache all assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: clean old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: cache-first strategy (works offline)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
